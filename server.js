bbff49fb9bb296dd1beb6f29b306d021508a2970
Orvin Welchez <owelchez@gmail.com>
2021-01-14 15:17:52 -0500

    Headers error on multiple res is fixed now

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const session = require('express-session');
 const expressValidator = require('express-validator');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
@@ -11,7 +12,14 @@ const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
 // I'm using only one db so I use mongoose.connect instead of createConnection
 const connection = require('./app/connection/mongoConnection.js');
+
+// const db = require('./app/config/keys').mongoURI;
+//
+// mongoose.connect(db, { useNewUrlParser: true, useUnifiedTopology: true }).then(() => console.log('Connection successful')).catch(err => console.log(err));
+
 const User = require('./app/models/user.js');
+
+
 
 // Initialize app
 const app = express();
@@ -34,31 +42,36 @@ app.use(express.static(path.join(__dirname, 'public')))
 //app.use(express.static("public"));
 app.set('view engine', 'ejs');
 
+//Express Session
+app.use(session({
+    secret: 'mazingerz', // I need to move this to environment variable
+    cookie: { maxAge: 60000 },
+    //store: sequelizeSessionStore,
+    saveUninitialized: false,
+    resave: false
+}));
+
 // Passport authentication
 app.use(cookieParser());
-
-// Express Session
-// app.use(session({
-//     secret: 'mazingerz', // I need to move this to environment variable
-//     cookie: { maxAge: 60000 },
-//     //store: sequelizeSessionStore,
-//     saveUninitialized: false,
-//     resave: false
-// }));
-
 app.use(passport.initialize());
 app.use(passport.session());
-//app.use(flash());
-
 app.use(flash());
 
-// Global variables
-// app.use(function(req, res, next){
-//   res.locals.success_msg = req.flash('success_msg');
-//   res.locals.error_msg = req.flash('error_msg');
-//   res.locals.error = req.flash('error');
-//   next();
-// });
+// Custom flash middleware -- from Ethan Brown's book, 'Web Development with Node & Express'
+app.use(function(req, res, next){
+    // if there's a flash message in the session request, make it available in the response, then delete it
+    res.locals.sessionFlash = req.session.sessionFlash;
+    delete req.session.sessionFlash;
+    next();
+});
+
+//Global variables
+app.use(function(req, res, next){
+  res.locals.success_msg = req.flash('success_msg');
+  res.locals.error_msg = req.flash('error_msg');
+  res.locals.error = req.flash('error');
+  next();
+});
 
 // Use our routes
 // const pages = ['/', 'signup', 'signin', 'findall'];
